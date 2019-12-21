05c74ede73b4a7d569b734d9650da3aa83bbc820
fantoro <fantoro@outlook.com>
2019-12-21 11:54:31 +0100

    Revert "Check for errors after initializing BEntry."
    This reverts commit 8e9cd6ec4a7d9d81d5d3bdf99134f7871707a793.

--- source/NoteRefFilter.cpp
+++ source/NoteRefFilter.cpp
@@ -6,8 +6,6 @@
 #include <Entry.h>
 #include <Node.h>
 #include <NodeInfo.h>
-#include <stdio.h>
-#include <MimeType.h>
 
 // Constructor
 NoteRefFilter :: NoteRefFilter() {
@@ -21,26 +19,26 @@ bool NoteRefFilter :: Filter(const entry_ref *ref,
 
 	// Follow links
 	BEntry entry(ref,true);
-	if(entry.InitCheck() != B_OK)
-		return false;
 
 	// Return true if the file is a directory
 	if (entry.IsDirectory())
 		return true;
 
 	// Create a BNode and a BNodeInfo to get the MIME type
-	entry_ref fRef;
-	if(entry.GetRef(&fRef) != B_OK)
-		return false;
+	BNode n(&entry);
+	BNodeInfo ninfo(&n);
 
-	// Create a BMimeType object to hold the MIME type
-	BMimeType m;
+	// Create a string to hold the MIME type
+	BString m;
 
-	// Guess the MIME type and store it in m
-	if(BMimeType::GuessMimeType(&fRef, &m))
-		return false;
+	// Lock the buffer's string to use with BNodeInfo::GetType
+	char *mbuf = m.LockBuffer(B_MIME_TYPE_LENGTH);
 
-	printf("%s\n", m.Type());
+	// Get MIME type
+	ninfo.GetType(mbuf);
+
+	// Unlock the string's buffer for use 
+	m.UnlockBuffer();
 
 	// Return true if the MIME type matches
 	if (m == "application/takenotes")
